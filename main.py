b2c7d31900d9364bf498d9fc2504674708733775
seunghoon4176 <ssh4176@naver.com>
2025-08-07 15:18:36 +0900

    delete ffiles

--- main.py
+++
@@ -1,9 +0,0 @@
-import sys
-from unified_launcher import main as unified_launcher_main
-
-def main():
-    """메인 함수 - 통합 게임 런처를 시작합니다."""
-    unified_launcher_main()
-
-if __name__ == "__main__":
-    main()
